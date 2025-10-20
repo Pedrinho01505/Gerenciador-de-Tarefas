367afce0741ec091235251f262e808e8710f6ee6
Pedro Henrique <pedrinhoavare18@gmail.com>
2025-10-20 16:05:35 -0300

    Inserindo tarefas na lista

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,12 @@
 function AddTask() {
-    return 
+    return (
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input type="text" placeholder="Digite o texto da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" /> 
+            <input type="text" placeholder="Digite a descrição da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" />
+            <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
+        </div>
+    )
 }
 
 export default AddTask
+
