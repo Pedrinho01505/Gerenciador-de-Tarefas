23f2f0e6840525ae811bd3e793087b68db3943db
Pedro Henrique <pedrinhoavare18@gmail.com>
2025-11-17 12:29:27 -0300

    Ajuste final

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -16,7 +16,7 @@ function Tasks ({ tasks, onTaskClick, onDeleteTaskClick}) {
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
         {tasks.map((task) => (
             <li key={task.id} className="flex gap-2">
-                <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 w-full flex items-center gap-2 text-white p-2 rounded-md text-left ${task.isCompleted && "line-through"}`}> {/*Se está tarefa for concluída eu adiciono esse tachado no texto*/}
+                <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 w-full flex items-center gap-2 text-white p-2 rounded-md text-left ${task.isCompleted && "line-through"}`}> 
                     {task.isCompleted && <CheckIcon/>}
                     {task.title}
                 </button> 
